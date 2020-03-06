9a8d1a621bceea20ef7093244f12ea8284222093
Nagy Istvan <stiwielegrand@gmail.com>
2020-03-06 16:04:52 +0100

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `My first gatsby project`,
+    description: `New gatsby project to learn the ecosystem .`,
     author: `@gatsbyjs`,
   },
   plugins: [
